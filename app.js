a2ca4e56bcc326d5f1962ac9b4b35505b009e6c3
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-03-19 10:28:50 +0100

    pushing my guys

--- app.js
+++ app.js
@@ -47,7 +47,6 @@ const handleClick = () => {
 
 
 
-
 // Binding of 'this'
 const obj = {
     value: 42,
@@ -74,5 +73,63 @@ const arrowFunc = () => {
 arrowFunc(1, 2, 3);
 
 // Use as constructors (arrow functions cannot be used as constructors)
-const Foo = () => {};
-const foo = new Foo(); // TypeError: Foo is not a constructor
+// const Foo = () => {};
+// const foo = new Foo(); // TypeError: Foo is not a constructor
+
+
+// ES6 Variables
+let x = 10;
+x = 20; // Valid
+
+const y = 5;
+// y = 10; // Invalid, attempting to reassign a const variable
+
+// Block Scoping with let and const
+
+// Variables declared with let and const are block-scoped, 
+// meaning they are only accessible within the block they are defined in.
+// This is different from var, which is function-scoped or globally scoped.
+
+
+{
+    let blockScope = 'scopeme'
+}
+
+{
+    let z = 30;
+    const pi = 3.14;
+}
+// console.log(z); // ReferenceError: z is not defined
+// console.log(pi); // ReferenceError: pi is not defined
+
+
+let tems = 'sabi girl'
+console.log(tems);
+
+function greeting(name='world'){
+    console.log("Hello " + name + "!");
+}
+
+greeting("emmauel")
+greeting()
+
+// Destructuring Assignment
+
+// Destructuring assignment allows you to extract values 
+// from arrays or properties from objects and assign them to 
+// variables in a more concise way.
+
+const Jarvis = {
+    firstName : 'Iron',
+    lastName : 'Man'
+} 
+
+const {firstName, lastName} = Jarvis
+console.log(firstName);
+console.log(lastName);
+
+list = ['Asake', 'Nasboi', 'Balablue']
+
+let[ item1, item2, item3] = list
+
+console.log(item1)
